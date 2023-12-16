4b5674128e2ec0fb0493928c78acca3a6111624c
Eric McLean <eric.mclean@infermedica.com>
2023-12-16 14:14:59 -0500

    less 4.5 form validation

--- src/components/CreatePage.vue
+++ src/components/CreatePage.vue
@@ -2,6 +2,7 @@
     
 <div class="container mb-3">
     <form action="">
+        <div class="col">
         <div class="mb-3">
             <label for="" class="form-label">
                 Page Title
@@ -21,11 +22,40 @@
             rows="5"
             v-model="content"></textarea>
         </div>
-        <div class="mb-3">
-            <button class="btn btn-primary" 
-            @click.prevent="pageCreated({pageTitle, content})">
+      </div>
+        <div class="col">
+            <div class="mb-3">
+                <label for="" class="form-label">
+                    Link Text
+                </label>
+                <input type="text"
+                class="form-control"
+                v-model="linkText"
+                />
+            </div>
+            <div class="mb-3">
+                <label for="" class="form-label">
+                    Link URL
+                </label>
+                <input type="text"
+                class="form-control"
+                v-model="linkUrl"
+                />
+            </div>
+            <div class="mb-3">
+                <div class="form-check">
+                    <input class="form-check-input" type="checkbox"/>
+                    <label class="form-check-label" for="gridCheck1">Published</label>
+                </div>
+            </div>
+            <div class="mb-3">
+            <button 
+            class="btn btn-primary" 
+            :disabled="isFormInvalid"
+            @click.prevent="submitForm">
                 Create Page
             </button>
+        </div>
         </div>
     </form>
 
@@ -34,10 +64,34 @@
 <script>
 export default {
     props: ['pageCreated'],
+    computed:{
+        isFormInvalid(){
+            return !this.pageTitle || !this.content || !this.linkText || !this.linkUrl;
+        }
+    },
     data(){
         return {
             pageTitle: '',
-            content: ''
+            content: '',
+            linkText: '',
+            linkUrl: '',
+        }
+    },
+    methods: {
+        submitForm(){
+            if(!this.pageTitle || !this.content || !this.linkText || !this.linkUrl){
+                alert('Please fill the form.');
+                return;
+            }
+            this.pageCreated({
+                pageTitle: this.pageTitle,
+                content: this.content,
+                link:{
+                    text: this.linkText,
+                    url: this.linkUrl
+
+                }
+            })
         }
     }
 }
