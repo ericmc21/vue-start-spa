7b43f8d8df2dbdc975b376c6bbce697cb1fb72db
Eric McLean <eric.mclean@infermedica.com>
2023-12-18 08:45:23 -0500

    lesson 4.7 watchers

--- src/components/CreatePage.vue
+++ src/components/CreatePage.vue
@@ -100,6 +100,13 @@ export default {
             this.linkUrl = '';
             this.published = true;
         }
+    },
+    watch:{
+        pageTitle(newTitle, oldTitle){
+            if(this.linkText == oldTitle ){
+                this.linkText = newTitle;
+            }
+        }
     }
 }
 </script>
