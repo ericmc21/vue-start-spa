22bba95881e303ffd11c6b6bc27272cf58fbd032
Eric McLean <eric.mclean@infermedica.com>
2023-12-19 09:50:09 -0500

    lesson 6.3 more routing and updating

--- src/components/PageViewer.vue
+++ src/components/PageViewer.vue
@@ -1,15 +1,25 @@
 <template>
-          <div  class="container">
-          <h1>Page Title</h1>
-          <p>{{$route.params.index}}</p>
+          <div v-if="page"  class="container">
+          <h1 class="emphasize">{{ page.pageTitle }}</h1>
+          <p>{{ page.content}}</p>
         </div>
 </template>
 
 <script>
 export default {
   created(){
-   // console.log(this.$route.params)
+   this.page = this.$pages.getSinglePage(this.$route.params.index);
 
+  },
+  data () {
+    return {
+    page: null
+    };
   }
 }
 </script>
+<style scoped>
+.emphasize {
+  color: blue;
+}
+</style>
