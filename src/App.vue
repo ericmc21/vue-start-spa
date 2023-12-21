ded0b96086ec6a19eddb4ca419da7a94b3177325
Eric McLean <eric.mclean@infermedica.com>
2023-12-21 13:56:33 -0500

    lession 6.6, nested routes

--- src/App.vue
+++ src/App.vue
@@ -2,23 +2,14 @@
   <navbar></navbar>
 
   <router-view> </router-view>
-  <!--   <page-viewer v-if="pages.length > 0" :page="pages[activePage]"></page-viewer>
-
-<create-page @page-created="pageCreated">
-  
-</create-page>
---></template>
+</template>
 
 <script>
 import Navbar from "./components/Navbar.vue";
-import PageViewer from "./components/PageViewer.vue";
-import CreatePage from "./components/CreatePage.vue";
 
 export default {
   components: {
     Navbar,
-    PageViewer,
-    CreatePage,
   },
   methods: {
     pageCreated(pageObj) {
