058bdaff9804efb74b1f53b0bfb1e6a0f5c17455
Eric McLean <eric.mclean@infermedica.com>
2023-12-27 09:34:20 -0500

    7.5 completing the edit page

--- src/main.js
+++ src/main.js
@@ -7,8 +7,9 @@ import $pages from "./data";
 
 const app = createApp(App);
 app.use(router);
-app.config.globalProperties.$bus = $bus;
+//app.config.globalProperties.$bus = $bus;
 //app.config.globalProperties.$pages = $pages;
 
 app.provide("$pages", $pages);
+app.provide("$bus", $bus);
 app.mount("#app");
--- src/views/PageEdit.vue
+++ src/views/PageEdit.vue
@@ -1,8 +1,76 @@
-<template>edit {{ route.params.index }}</template>
+<template>
+  <h4>Edit {{ page.pageTitle }}</h4>
+  <form action="" class="container mb-3">
+    <div class="row">
+      <div class="col-md-8">
+        <div class="mb-3">
+          <label for="" class="form-label"> Page Title </label>
+          <input type="text" class="form-control" v-model="page.pageTitle" />
+        </div>
+        <div class="mb-3">
+          <label for="" class="form-label"> Content </label>
+          <textarea
+            type="text"
+            class="form-control"
+            rows="5"
+            v-model="page.content"
+          ></textarea>
+        </div>
+      </div>
+      <div class="col">
+        <div class="mb-3">
+          <label for="" class="form-label"> Link Text </label>
+          <input type="text" class="form-control" v-model="page.link.text" />
+        </div>
+
+        <div class="mb-3">
+          <div class="form-check">
+            <input
+              class="form-check-input"
+              type="checkbox"
+              v-model="page.published"
+            />
+            <label class="form-check-label" for="gridCheck1">Published</label>
+          </div>
+        </div>
+        <div class="mb-3">
+          <button
+            class="btn btn-primary me-2"
+            @click.prevent="submit"
+            :disabled="isFormInvalid"
+          >
+            Edit
+          </button>
+          <button class="btn btn-secondary" @click.prevent="goToPagesList">
+            Cancel
+          </button>
+        </div>
+      </div>
+    </div>
+  </form>
+</template>
 
 <script setup>
-import { useRoute } from "vue-router";
-const route = useRoute();
+import { useRouter } from "vue-router";
+import { inject } from "vue";
 
-const props = defineProps(["index"]);
+const router = useRouter();
+const pages = inject("$pages");
+const bus = inject("$bus");
+
+const { index } = defineProps(["index"]); // destructure prop to get direct access to index
+let page = pages.getSinglePage(index);
+
+function submit() {
+  pages.editPage(index, page);
+  bus.$emit("page-updated", {
+    index,
+    page,
+  });
+  goToPagesList();
+}
+
+function goToPagesList() {
+  router.push({ path: "/pages" });
+}
 </script>
